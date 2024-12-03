d8b974a764b2496ccf05207d8821d1b3f7e31a4c
Rahul Ramesh <rahul.ramesh@thoughtworks.com>
2024-12-03 07:48:21 +0530

    [Rahul] | Add. Database Config

---
+++ backend/app/config/database.py
@@ -0,0 +1,28 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+load_dotenv()
+
+SQL_ALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(SQL_ALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+def create_tables(bases: list[declarative_base]):
+    for base in bases:
+        base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
