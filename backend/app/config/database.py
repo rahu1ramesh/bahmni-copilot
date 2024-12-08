671132a5ab23d548d8e83cb183a0f63b8e6e961a
Rahul Ramesh <rahul.ramesh@thoughtworks.com>
2024-12-08 18:48:03 +0530

    [Rahul] | Refactor. Rename engine -> db_engine

--- backend/app/config/database.py
+++ backend/app/config/database.py
@@ -7,16 +7,16 @@ load_dotenv()
 
 SQL_ALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(SQL_ALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+db_engine = create_engine(SQL_ALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=db_engine)
 
 Base = declarative_base()
 
 
-def create_tables(bases: list[declarative_base]):
+def create_tables(bases: list[declarative_base], db_engine):
     for base in bases:
-        base.metadata.create_all(bind=engine)
+        base.metadata.create_all(bind=db_engine)
 
 
 def get_db():
