552de191546b5974c2c6a62f7d1f464e5a2d3ac6
Rahul Ramesh <rahul.ramesh@thoughtworks.com>
2024-12-27 09:12:31 +0530

    [Rahul] | Add. ESLint Config

---
+++ frontend/eslint.config.js
@@ -0,0 +1,35 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      "react/jsx-uses-react": "off",
+      "react/react-in-jsx-scope": "off"
+    }
+  },
+  {
+    ignores: [
+      ".gitignore",
+      ".prettierrc.cjs",
+      "/tailwind.config.cjs",
+      "build/"
+    ]
+  },
+  {
+    settings: {
+      react: {
+        version: "detect"
+      }
+    }
+  }
+];
