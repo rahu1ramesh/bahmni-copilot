c64791d004e364a098f55fedec859acfe1e53a24
Rahul Ramesh <rahul.ramesh@thoughtworks.com>
2024-12-27 13:09:58 +0530

    [Rahul] | Fix. Incorrent Route Import

--- backend/app/api/routes/__init__.py
+++ backend/app/api/routes/__init__.py
@@ -4,7 +4,6 @@ from app.api.routes.departments import router as departments_router
 from app.api.routes.forms import router as forms_router
 from app.api.routes.fields import router as fields_router
 from app.api.routes.patients import router as patients_router
-from app.api.routes.references import router as references_router
 from app.api.routes.users import router as users_router
 from app.api.routes.transcriptions import router as transcriptions_router
 
@@ -15,6 +14,5 @@ router.include_router(departments_router)
 router.include_router(forms_router)
 router.include_router(fields_router)
 router.include_router(patients_router)
-router.include_router(references_router)
 router.include_router(users_router)
 router.include_router(transcriptions_router)
